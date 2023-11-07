0fcca3b7909ded1c76117909a47ab58664a0f01b
amezquitam <amezquitamiguel7@gmail.com>
2023-11-07 18:37:22 -0500

    agregando restricción: lanchero

---
+++ a..js
@@ -0,0 +1,15 @@
+class User {
+    /**
+     * 
+     * @param firstname: String, primer nombre del usuario
+     * @param lastname: String, apellido del usuario
+     * @param username: String, nombre de usuario
+     * @param password: String, contraseña del usuario
+     */
+    constructor(firstname, lastname, username, password) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.username = username
+        this.password = password
+    }
+}
