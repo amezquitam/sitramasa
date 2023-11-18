2fd60df15bf8bc6a8eb8423794e55bb3409915b8
amezquitam <amezquitamiguel7@gmail.com>
2023-11-18 13:21:48 -0500

    update

---
+++ client/src/views/common/LoadingPage.jsx
@@ -0,0 +1,18 @@
+import './LoadingPage.css'
+
+export default function LoadingPage() {
+    return (
+        <div className="container">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
